0793f67118c77040523ea5717c054cb8e39be057
Josh Ward <joshwardcodes@gmail.com>
2025-05-18 20:45:43 -0700

    Do dumb basic mining game timer

--- src/game/mining_area.gd
+++ src/game/mining_area.gd
@@ -1,7 +1,27 @@
 extends Node2D
 
 @export var input_context: GUIDEMappingContext
+@export var total_mining_time: float = 60
+
+@onready var time_elapsed: float = 0
+@onready var time_left: float = total_mining_time
 
 
 func _ready():
 	GUIDE.enable_mapping_context(input_context, true)
+
+
+func _process(delta: float) -> void:
+	time_elapsed += delta
+	time_left = max(total_mining_time - time_elapsed, 0)
+
+	# temp - set time left
+	%TimeLeftLabel.text = "Time left: %.0f" % time_left
+
+	if time_left <= 0:
+		_end_mining()
+
+
+func _end_mining() -> void:
+	# todo - actually do something when ending mining session
+	get_tree().paused = true
