8c1478a42d2e897d09083ddd502d149db40af004
SpencerTyler <156151736+SpencerTyler@users.noreply.github.com>
2025-05-21 15:26:13 -0700

    Mining start & end screens (#63)
    * Linked to main menu, added start and end Screens
    
    * Added UI to End Screens
    
    ---------
    
    Co-authored-by: Spencer Tyler <Stylercode@gmail.com>

---
+++ src/game/end_mining.gd
@@ -0,0 +1,23 @@
+class_name EndMining
+extends CanvasLayer
+
+@export var chip_labels: Dictionary [Enum.MiningResourceType, Label]
+
+
+func _on_button_pressed() -> void:
+	State.Scene.active_scene = "res://main_menu/main_menu.tscn"
+	get_tree().paused = false
+
+
+func on_time_up():
+	get_tree().paused = true
+	visible = true
+
+	var chip_resources = State.Inventory.get_all_staged_mining_resources()
+	chip_resources.get(Enum.MiningResourceType.BLUE_CHIP, 0)
+
+	Service.Inventory.bank_staged_mining_resources()
+
+
+func update_chips(resource_type: Enum.MiningResourceType, quantity: int):
+	chip_labels[resource_type].text = str(quantity)
