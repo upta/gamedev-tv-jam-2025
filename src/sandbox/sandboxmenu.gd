0e42d4334f0076be7ff38e948fe772fc8c7a129b
Christopehr Seidel <seidel@live.com>
2025-05-20 19:33:59 -0700

    Added Main Menu background and button themes

--- src/sandbox/sandboxmenu.gd
+++ src/sandbox/sandboxmenu.gd
@@ -1,12 +1,7 @@
 extends Control
 
-@export var input_context: GUIDEMappingContext
-@export var start_action: GUIDEAction
-
 
 func _ready() -> void:
-	GUIDE.enable_mapping_context(input_context, true)
-	start_action.triggered.connect(_on_start_action_triggered)
 
 	# get size scale to 1024x1024
 	var viewPort = get_viewport().get_visible_rect().size
@@ -21,6 +16,7 @@ func _ready() -> void:
 	var xUIScale = (xScale) * 250
 	var yUIScale = (yScale) * 125
 
+
 	prints(xUIScale, yUIScale)
 	$butNew.size = Vector2(xUIScale, yUIScale)
 	$butNew.position = Vector2(5, 5)
@@ -30,28 +26,3 @@ func _ready() -> void:
 	$butOpt.position = Vector2(5, yUIScale + 5)
 	$butQuit.size = Vector2(xUIScale, yUIScale)
 	$butQuit.position = Vector2(xUIScale + 10, yUIScale + 5)
-
-
-func _on_start_action_triggered():
-	pass#State.Scene.active_scene = "res://main_menu/level_select/LevelSelect.tscn"
-
-
-func _on_but_new_pressed() -> void:
-	State.Scene.active_scene = "res://game/mining_area.tscn"
-
-
-func _on_but_opt_pressed() -> void:
-	prints("options button")
-
-
-func _on_but_quit_pressed() -> void:
-	get_tree().quit()
-
-
-func _on_but_sel_pressed() -> void:
-	State.Scene.active_scene = "res://main_menu/level_select/LevelSelect.tscn"
-
-
-func _on_but_opt_toggled(toggled_on: bool) -> void:
-	prints("Toggled")
-	# Replace with function body.
