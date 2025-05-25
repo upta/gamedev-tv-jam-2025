8a52e8804a7c1b16e17cd4db88090bdb314c0176
Brian <upta@outlook.com>
2025-05-25 15:01:42 -0700

    Improves market price display with animations
    Refactors the market price line item to display price fluctuations with up/down arrow animations.

--- src/home_base/market_price_line_item.gd
+++ src/home_base/market_price_line_item.gd
@@ -1,16 +1,18 @@
 class_name MarketPriceLineItem extends HBoxContainer
 
+var coin: CoinResource
+
 @onready var icon: TextureRect = %Icon
 @onready var coin_name: Label = %CoinName
 @onready var price: Label = %Price
-@onready var direction: TextureRect = %Direction
+@onready var direction_sprite: AnimatedSprite2D = $Direction/DirectionSprite
 
 
-func activate(coin: CoinResource):
-	icon.texture = coin.icon
-	coin_name.text = coin.name
+func _on_price_changed(coin_type: Enum.CoinType, _price: float):
+	if coin.type != coin_type:
+		return
 
-	var price_history = State.Market.prices[coin.type]
+	var price_history = State.Market.prices[coin_type]
 	price.text = "%.02f" % price_history.current_price
 
 	var history_size = price_history.history.size()
@@ -21,4 +23,16 @@ func activate(coin: CoinResource):
 		var previous_index = history_size - 2
 		is_up = price_history.current_price > price_history.history[previous_index]
 
-	direction.flip_v = !is_up
+	if is_up:
+		direction_sprite.play("up")
+	else:
+		direction_sprite.play("down")
+
+
+func activate(_coin: CoinResource):
+	coin = _coin
+	icon.texture = coin.icon
+	coin_name.text = coin.name
+
+	_on_price_changed(coin.type, 0.0)
+	State.Market.price_changed.connect(_on_price_changed)
