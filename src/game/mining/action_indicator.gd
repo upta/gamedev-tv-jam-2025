57862a665a5a8cbb1dc0a23c52caf72b61c7d12f
Josh <joshwardcodes@gmail.com>
2025-05-25 08:16:42 -0700

    Various upgrade and mining fixes (#132)
    * change to new upgrade images
    
    * decrease the upgrade cost by 1/10 to help normalize more
    
    * Adjust special action bar size, add hot key indicator
    
    * Fix loud mining sound. Add missing drop bomb sound

--- src/game/mining/action_indicator.gd
+++ src/game/mining/action_indicator.gd
@@ -2,17 +2,21 @@ class_name ActionIndicator
 extends Control
 
 @export var icon: CompressedTexture2D
+@export var hot_key: String
 
 
 func _ready() -> void:
 	$Icon.texture = icon
+	$KeyLabel.text = hot_key
 
 
 func set_time_left(time: float):
 	if time > 0:
 		$Icon.self_modulate.a = 0.5
-		$Label.show()
-		$Label.text = str(ceili(time))
+		$KeyLabel.hide()
+		$CountDownLabel.show()
+		$CountDownLabel.text = str(ceili(time))
 	else:
 		$Icon.self_modulate.a = 1
-		$Label.hide()
+		$CountDownLabel.hide()
+		$KeyLabel.show()
