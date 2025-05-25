b6860fc2bf75842543241c7bdec3f1baebb745b8
Brian <upta@outlook.com>
2025-05-25 11:51:40 -0700

    Refactors mining door interaction
    Updates the mining door interaction pause the game during refunding, and play a sound effect.
    
    Also returns the held coin

--- src/home_base/mining_door.gd
+++ src/home_base/mining_door.gd
@@ -8,11 +8,28 @@ func _ready():
 
 
 func _on_interacted():
-	for resource_type in ItemTypes.mining_resources:
-		var remaining = State.Inventory.mining_resources.get(resource_type, 0)
-		for i in range(0, remaining):
-			Service.Inventory.refund_resource(resource_type)
-			await get_tree().create_timer(0.25).timeout
+	State.Inventory.held_coin_type = Enum.CoinType.NONE
+
+	if _has_remaining_chips():
+		get_tree().paused = true
+		for resource_type in ItemTypes.mining_resources:
+			var remaining = State.Inventory.mining_resources.get(resource_type, 0)
+			for i in range(0, remaining):
+				Service.Inventory.refund_resource(resource_type)
+				AudioService.making_power.play()
+
+				await get_tree().create_timer(0.1).timeout
+		get_tree().paused = false
 
 	State.Scene.active_scene = "res://game/mining_area.tscn"
 	AudioService.game_music_home.stop()
+
+
+func _has_remaining_chips() -> bool:
+	for resource_type in ItemTypes.mining_resources:
+		var remaining = State.Inventory.mining_resources.get(resource_type, 0)
+
+		if remaining > 0:
+			return true
+
+	return false
