a305619528ef22949b9acc9aadc9faf27d6e598b
Christopher Seidel <seidel@live.com>
2025-05-25 10:53:08 -0700

    Refactored dirt. (#141)
    * Refactored dirt.
    
    * update dirt to use destructible images
    
    * Fix tutorial, update sprite choice a little
    
    ---------
    
    Co-authored-by: Josh Ward <joshwardcodes@gmail.com>

--- src/game/mining/blocks/dirt.gd
+++ src/game/mining/blocks/dirt.gd
@@ -2,21 +2,25 @@ extends Mineable
 
 var dirt_color: Color
 
-@onready var dirt_sprite: Sprite2D = $DirtSprite
+@onready var dirt_sprite: AnimatedSprite2D = $DirtSprite
 @onready var sfx_mined: AudioStreamPlayer2D = $MinedSFX
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
 
 func _ready() -> void:
-	var color_val = 1 - (durability - 1) * 0.2
-	dirt_color = Color(color_val, color_val, color_val)
-	dirt_sprite.modulate = dirt_color
+	dirt_sprite.rotate(randi_range(0, 3) * PI / 4)
+	_set_frame()
+
+
+func _set_frame():
+	dirt_sprite.frame = clampi(durability - 1, 0, 4)
 
 
 func on_damaged():
 	dirt_sprite.modulate = Color.RED
 	await get_tree().create_timer(0.1).timeout
-	dirt_sprite.modulate = dirt_color
+	dirt_sprite.modulate = Color.WHITE
+	_set_frame()
 
 
 func on_mined():
