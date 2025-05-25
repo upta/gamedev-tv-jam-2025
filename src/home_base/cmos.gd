2bb93fbbfc054e7b78ceea016333b4e8dc922851
James Milholland <james.milholland@gmail.com>
2025-05-24 19:32:09 -0700

    CMOS battery

---
+++ src/home_base/cmos.gd
@@ -0,0 +1,23 @@
+extends AnimatedSprite2D
+
+@onready var current_power: Label = %CurrentPower
+
+
+func _ready():
+	State.Inventory.power_changed.connect(_on_power_changed)
+	_on_power_changed(State.Inventory.power)
+
+
+func _on_power_changed(power: float):
+	# Calculate ratio (0 to 1)
+	var ratio = power / State.Inventory.MAX_POWER
+
+	#Clamp to valid range
+	ratio = clamp(ratio, 0.0, 1.0)
+
+	#Round to nearest 10 %
+	var rounded_ratio = floori(ratio * 10.0) * 10
+
+	play(str(rounded_ratio))
+
+	current_power.text = "%.02f" % power
